300cad513ce0ee0939a96fae3b481e90e70f9c96
Merveille van Eck <merveillevaneck@gmail.com>
2021-06-26 10:57:22 +0200

    refactor: changed route names to camel case

--- src/App.js
+++ src/App.js
@@ -20,15 +20,15 @@ function App() {
       <Navbar />
       <Switch>
         <Route exact path="/" exact component={Home} />
-        <Route exact path="/About" component={About} />
-        <Route exact path="/Projects" component={Projects} />
-        <Route exact path="/Contact" component={Contact} />
-        <Route exact path="/Scrumpro" component={Scrumpro} />
-        <Route exact path="/Playpro" component={Playpro} />
-        <Route exact path="/NectaTrack" component={NectaTrack} />
-        <Route exact path="/Necta" component={Necta} />
-        <Route exact path="/BloomWeddings" component={BloomWeddings} />
-        <Route exact path="/ThankYouCard" component={ThankYouCard} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/scrumpro" component={Scrumpro} />
+        <Route exact path="/playpro" component={Playpro} />
+        <Route exact path="/necta-track" component={NectaTrack} />
+        <Route exact path="/necta" component={Necta} />
+        <Route exact path="/bloom-weddings" component={BloomWeddings} />
+        <Route exact path="/thank-you" component={ThankYouCard} />
       </Switch>
       <Footer />
     </Router>
